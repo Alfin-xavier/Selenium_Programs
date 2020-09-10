039789904fecea911211ab80c9b79fcbb22daf0b
ATMECS\Alfin.Anthonyraj <alfin.anthonyraj@atmecs.com>
2020-09-10 18:41:42 +0530

    Updated

--- src/main/java/com/atmecs/assignments/Selenium/Assignmant1.java
+++ src/main/java/com/atmecs/assignments/Selenium/Assignmant1.java
@@ -6,7 +6,6 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.*;
-import org.testng.asserts.*;
 
 public class Assignmant1 
 {
