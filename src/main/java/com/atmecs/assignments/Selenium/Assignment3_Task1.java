c53f696249d410a81d35df37c2d19e7935e1c418
ATMECS\Alfin.Anthonyraj <alfin.anthonyraj@atmecs.com>
2020-09-15 10:59:23 +0530

    Updated

--- src/main/java/com/atmecs/assignments/Selenium/Assignment3_Task1.java
+++ src/main/java/com/atmecs/assignments/Selenium/Assignment3_Task1.java
@@ -1,15 +1,14 @@
 package com.atmecs.assignments.Selenium;
 
 import java.util.Iterator;
+import java.util.List;
 import java.util.Set;
 import java.util.concurrent.TimeUnit;
-
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.*;
 
 public class Assignment3_Task1 
@@ -23,11 +22,13 @@ public class Assignment3_Task1
 		driver = new ChromeDriver();
 		driver.get("https://www.makemytrip.com/");
 		driver.manage().window().maximize();
+		
 	}
 	
 	@Test(priority=1)
 	public void handlingWindowsViaSignup() throws InterruptedException
 	{
+		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
 		driver.findElement(By.cssSelector("li.makeFlex.hrtlCenter.font10.makeRelative.lhUser")).click();
 		Thread.sleep(2000);
 		driver.findElement(By.cssSelector("span.font14.darkGreyText.latoBold")).click();
@@ -42,24 +43,77 @@ public class Assignment3_Task1
 		Thread.sleep(2000);
 		WebElement password=driver.findElement(By.xpath("//div[@class='aXBtI I0VJ4d Wic03c']//div//input[@name='password']"));
 		password.sendKeys("alfinxavier.a");
-		
 		WebElement next2=driver.findElement(By.cssSelector("button.VfPpkd-LgbsSe.VfPpkd-LgbsSe-OWXEXe-k8QpJ.VfPpkd-LgbsSe-OWXEXe-dgl2Hf.nCP5yc.AjY5Oe.DuMIQc.qIypjc.TrZEUc"));
 		next2.click();
-		Thread.sleep(2000);
+		 Thread.sleep(1000); 
+		 
 	}
 	
-	@Test(priority=2)
-	public void bookingTwoWayTrip() throws InterruptedException
-	{
-		//driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
-		WebElement roundtrip = driver.findElement(By.xpath("//div[@class='minContainer']//div//div//div[1]//ul//li[2]//span[@class='tabsCircle appendRight5']"));
+	  @Test(priority=2)
+	  public void bookingTwoWayTrip() throws InterruptedException
+	  { 
+		driver.manage().timeouts().implicitlyWait(7, TimeUnit.SECONDS); 
+		
+		// Checking the RoundTrip
+		
+		WebElement roundtrip = driver.findElement(By.xpath("//ul[@class='fswTabs latoBlack greyText']//li[2]")); 
 		if(roundtrip.isSelected()==false)
 			roundtrip.click();
 		Thread.sleep(1000);
-		//driver.findElement(By.cssSelector("input.react-autosuggest__input.react-autosuggest__input--open")).sendKeys("Tri");
-	}
+		
+		//From And To
+		
+		WebElement from = driver.findElement(By.xpath("//input[@id='fromCity']"));
+		from.sendKeys("Tri");
+		from.sendKeys(Keys.ARROW_DOWN);
+		from.sendKeys(Keys.ENTER);
+		Thread.sleep(1000);
+		
+		WebElement to=driver.findElement(By.xpath("//*[@id='toCity']"));
+		to.sendKeys("Chen");
+		to.sendKeys(Keys.ARROW_DOWN);
+		to.sendKeys(Keys.ENTER);
+		Thread.sleep(2000);
+		
+		//Depature & Return
+		
+		driver.findElement(By.xpath("//div[@class='fsw_inputBox dates inactiveWidget activeWidget']")).click();
+			driver.findElement(By.cssSelector("div.DayPicker-Day.DayPicker-Day--start.DayPicker-Day--selected.DayPicker-Day--today")).click();
+	  
+		driver.findElement(By.xpath("//div[@class='fsw_inputBox dates reDates inactiveWidget activeWidget shiftModal']")).click();
+		List<WebElement> web=driver.findElements(By.cssSelector("td.DayPicker-Day"));
+		int count=web.size();
+		for(int i=0;i<count;i++)
+		{
+			String dates=web.get(i).getText();
+			if(dates.equalsIgnoreCase("29"))
+			{
+				web.get(i).click();
+			}
+		}
+		
+		//Travellers & classes
+		
+		driver.findElement(By.id("travellers")).click();
+		
+		driver.findElement(By.xpath("//div[@class='appendBottom20']//ul//li[@data-cy='adults-2']")).click();
+		driver.findElement(By.xpath("//div[@class='makeFlex column childCounter']//ul//li[@data-cy='children-1']")).click();
+		driver.findElement(By.xpath("//div[@class='makeFlex column pushRight infantCounter']//ul//li[@data-cy='infants-0']")).click();
+	  
+		driver.findElement(By.xpath("//ul[@class='guestCounter classSelect font12 darkText']//li[2]")).click();
+		driver.findElement(By.xpath("//button[@class='primaryBtn btnApply pushRight ']")).click();
+		
+		//Clicking the Search Button
+		
+		driver.findElement(By.className("primaryBtn font24 latoBold widgetSearchBtn ")).click();
+	  }
+	 
 	
-	/*
-	 * @AfterTest public void closingDriver() { driver.close(); }
-	 */
+	
+	  @AfterTest 
+	  public void closingDriver()
+	  { 
+		  driver.close(); 
+	  }
+	 
 }
